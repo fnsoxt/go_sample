b503a2bd64d0956395a818b7b00b1c17ce81bdbd
zhangzhijie <zhangzhijie@haohaozhu.com>
2019-03-01 18:10:17 +0800

    xiaomi

---
+++ push/xiaomi.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"github.com/yilee/xiaomi-push"
+)
+
+
+func main() {
+	var client = xiaomipush.NewClient("Y2Gqtk49HSXzSc4FM1pkvA==", []string{"com.hzhu.m"})
+	var msg1 *xiaomipush.Message = xiaomipush.NewAndroidMessage("title", "body").SetPayload("this is payload1")
+	res, err := client.SendToAlias(context.Background(), msg1, "5608575")
+	fmt.Println(res, err)
+}
