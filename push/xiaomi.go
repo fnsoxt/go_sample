ded94de91226cf734ecc2d4a5d79e4a7eabc5b86
zhangzhijie <zhangzhijie@haohaozhu.com>
2019-03-04 16:25:18 +0800

    update

--- push/xiaomi.go
+++ push/xiaomi.go
@@ -6,7 +6,6 @@ import (
 	"github.com/yilee/xiaomi-push"
 )
 
-
 func main() {
 	var client = xiaomipush.NewClient("Y2Gqtk49HSXzSc4FM1pkvA==", []string{"com.hzhu.m"})
 	var msg1 *xiaomipush.Message = xiaomipush.NewAndroidMessage("title", "body").SetPayload("this is payload1")
