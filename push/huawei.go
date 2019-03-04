97d065184a29ad67ae5f6d9d748329b771fbe94e
zhangzhijie <zhangzhijie@haohaozhu.com>
2019-03-04 16:42:56 +0800

    huawei

--- push/huawei.go
+++ push/huawei.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	client := huaweipush.NewClient("100307583", "01b8c58558f3e1f2aa8d8b633308a986")
-	res, err := client.SingleSend(context.TODO(), huaweipush.NewSingleNotification("0861331032881895300000533500CN01", "message").SetRequestID("requestID").SetHighPriority())
+	singleNotification := huaweipush.NewSingleNotification("0861331032881895300000533500CN01", "message").SetRequestID("requestID").SetHighPriority()
+	res, err := client.SingleSend(context.TODO(), singleNotification)
 	fmt.Println(res, err)
 }
