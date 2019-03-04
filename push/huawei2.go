5028bc587a028708118733e189ea6f8e2b823308
zhangzhijie <zhangzhijie@haohaozhu.com>
2019-03-04 17:04:57 +0800

    huawei

--- push/huawei2.go
+++ push/huawei2.go
@@ -6,17 +6,17 @@ import (
 )
 
 func main() {
-	ClientId := "***"
-	ClientSecret := "***"
+	ClientId := "100307583"
+	ClientSecret := "01b8c58558f3e1f2aa8d8b633308a986"
 	client := huawei.NewClient(ClientId, ClientSecret)
 
 	// getToken
 	// accessToken := client.GetToken()
 	// fmt.Println("accessToken", accessToken)
 
-	// push msg(会自己去 getToken 再请求推送)
-	token := "***"
-	payload := huawei.NewMessage().SetContent("huawei-content").SetTitle("huawei-title").Json()
+	token := "0861331032881895300000533500CN01"
+	payload := huawei.NewMessage().SetContent("huawei-content").SetTitle("huawei-title").SetIntent("com://hzhu.m.logo/openwith?link=%s&push_id=%s").SetAppPkgName("com.hzhu.m").Json()
+	fmt.Println(payload)
 	result := client.PushMsg(token, payload)
 	fmt.Println("result", result)
 }
