f173e1327dbf67b26821ba977b51d36364edd1a6
zhangzhijie <zhangzhijie@haohaozhu.com>
2019-03-12 11:27:45 +0800

    update

--- push/xiaomi.go
+++ push/xiaomi.go
@@ -9,6 +9,6 @@ import (
 func main() {
 	var client = xiaomipush.NewClient("Y2Gqtk49HSXzSc4FM1pkvA==", []string{"com.hzhu.m"})
 	var msg1 *xiaomipush.Message = xiaomipush.NewAndroidMessage("title", "body").SetPayload("this is payload1")
-	res, err := client.SendToAlias(context.Background(), msg1, "5608575")
+	res, err := client.SendToAlias(context.Background(), msg1, "5434710")
 	fmt.Println(res, err)
 }
